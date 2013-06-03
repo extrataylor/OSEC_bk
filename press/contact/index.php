85a546d225beafce5891f3272d2f6e9943782e52
Eric <extrataylor@gmail.com>
2013-06-03 18:44:23 -0400

    Revert "deleted"
    This reverts commit 47fd1060ac0b0a358e447c1e2eebd262c94ffac9.

---
+++ press/contact/index.php
@@ -0,0 +1,19 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/inc/top_nolist.inc"); ?>
+<div id="container">
+			<div id="contact">
+				<p><span><h12>Occupy the SEC</h12></span> is a group of concerned citizens, activists, and financial 
+				professionals with decades of collective experience working at many of the 
+				largest financial firms in the industry. Together, we make up a vast array 
+				of specialists, including traders, quantitative analysts, compliance 
+				officers, and technology and risk analysts. Like much of the 99%, we have 
+				bank deposits and retirement accounts that are in need of protection 
+				through vigorous enforcement of the financial markets.</p>
+				<br>
+				We can be reached at occupythesec@gmail.com.
+				
+								
+			</div>
+		</div>
+		<?php include($_SERVER['DOCUMENT_ROOT']."/inc/contactbottom.inc"); ?>
+	</body>
+</html>
